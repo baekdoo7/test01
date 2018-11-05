b68ecefa8fc6341e51fa2f04a2829d8792369682
baekdoo <baekdoo@adop.co.kr>
2018-11-05 16:19:26 +0900

    crawler work

--- AMP/tagtest.php
+++ AMP/tagtest.php
@@ -22,6 +22,7 @@
 </head>
 
 <body>
+
 <!-- Google Tag Manager (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KF4TCR6"
                   height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
